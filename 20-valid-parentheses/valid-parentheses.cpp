10067eb1a136bdc88f4f07a7f5884b105b8373ee
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-07 15:50:05 +0530

    Time: 2 ms (14.99%) | Memory: 10 MB (6.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    //swapnil
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i] == '{' || s[i] == '[' ){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+
+                if(st.top()=='(' && s[i]==')'){
+                    st.pop();
+                }else if(st.top()=='{' && s[i]=='}'){
+                    st.pop();
+                }else if(st.top()=='[' && s[i]==']'){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
