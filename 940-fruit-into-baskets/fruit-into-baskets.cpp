34c372dbf3d7fbd4c55a8cc597bfee3ee99f3e05
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-12 15:57:41 +0530

    Time: 44 ms (53.96%) | Memory: 84.9 MB (34.16%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -9,17 +9,27 @@ public:
         while(j<n){
             mpp[fruits[j]]++;
             //O(N+M)
+            // if(mpp.size()>k){
+            //     while(mpp.size()>k){
+            //         mpp[fruits[i]]--;
+            //         if(mpp[fruits[i]]==0) mpp.erase(fruits[i]);
+            //         i++;
+            //     }
+            // }  
+            // if(mpp.size()<=k){
+            //     ans=max(ans,j-i+1);
+            // }
             if(mpp.size()>k){
-                while(mpp.size()>k){
+                if(mpp.size()>k){
                     mpp[fruits[i]]--;
                     if(mpp[fruits[i]]==0) mpp.erase(fruits[i]);
                     i++;
                 }
-            }
-            
+            }  
             if(mpp.size()<=k){
                 ans=max(ans,j-i+1);
             }
+
             j++;
         }
         return ans;
