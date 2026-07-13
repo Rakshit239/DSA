12770e5d30ee3a7199268298bb7940cb21c72e43
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-13 11:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.30%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
+//swapnil
     int const MOD=1e9+7;
     long long power(int x,long long n){
             if(n==0){
                 return 1;
             }
 
-            long long half = power(x,n/2);
-            long long ans= (half*half)%MOD;
-            if(n%2!=0){
-                ans=(x*ans)%MOD;
-            }
-            return ans;
+            // long long half = power(x,n/2);
+            // long long ans= (half*half)%MOD;
+            // if(n%2!=0){
+            //     ans=(x*ans)%MOD;
+            // }
+            // return ans;
+            if(n==0){
+            return 1;
+        }
+        if(n%2==0){
+            return power((1LL*x*x)%MOD,n/2);
+        }
+        
+        return (x*power((1LL*x*x)%MOD,(n-1)/2))%MOD;
     }
     int countGoodNumbers(long long n) {
         long long even=(n+1)/2;
