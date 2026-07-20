6c8dd02804ba0b36db892a351f6c68ab9cba2266
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-20 11:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (63.76%) - LeetSync

--- 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     string winningPlayer(int x, int y) {
-        // if(x%2==0 && y%4==3){
-        //     return "Alice";
-        // }
-        // return "Bob";
-
-        int count=0;
-        while(x>=1 && y>=4){
-            x-=1;
-            y-=4;
-            count++;
-        }
+        int count =(min(x,y/4));
         if(count&1){
             return "Alice";
         }
         return "Bob";
+        //O(n)
+        // int count=0;
+        // while(x>=1 && y>=4){
+        //     x-=1;
+        //     y-=4;
+        //     count++;
+        // }
+        // if(count&1){
+        //     return "Alice";
+        // }
+        // return "Bob";
     }
 };
