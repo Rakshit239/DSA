74fa63a49b8db1bdd4fd6c7e349dac2464c98ca0
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-21 09:56:32 +0530

    Time: 2 ms (28.83%) | Memory: 21.5 MB (27.98%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int gcd(int x,int y){
+        int a=max(x,y);
+        int b =min(x,y);
+        while(b!=0){
+            int temp = b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int,int> freq;
+        for(int num:deck){
+            freq[num]++;
+        }
+        int GCD;
+        for(auto it:freq){
+            GCD=gcd(GCD,it.second);
+            if(GCD==1) return false;
+        }
+        return true;
+        
+    }
+};
