1695988f5d4965f4a6e30eeee4cb8fb04e48ba57
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-22 09:33:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost to Connect Ropes/min-cost-to-connect-ropes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>> pq;
+        int sum=0;
+        for(int i :arr){
+            pq.push(i);
+        }
+        int ans=0;
+        while(pq.size()!=1){
+            int sum=0;
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            sum=a+b;
+            ans+=sum;
+            pq.push(sum);
+        }
+        return ans;
+        
+        
+    }
+};
