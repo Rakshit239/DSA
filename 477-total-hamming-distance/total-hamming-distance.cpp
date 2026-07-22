d6812c6e79fa6961c88d84249900f4beb32af731
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-22 14:49:32 +0530

    Time: 12 ms (58.81%) | Memory: 22.7 MB (82.29%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
         int sum=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                sum+=__builtin_popcount(nums[i]^nums[j]);
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         sum+=__builtin_popcount(nums[i]^nums[j]);
+        //     }
+        // 
+        
+        int total=0;
+        for(int i=0;i<32;i++){
+            int setbits=0;
+            int zero=0;
+            for(auto it:nums){
+                if(((it>>i)&1)==1) {
+                    setbits++;
+                }else{
+                    zero++;
+                }
             }
+            total+=(setbits*zero);
         }
-        return sum;
+        return total;
     }
 };
