acbadefa4b11413b6b2f50649d157e5d71c42c04
Rakshit Sethi <rakshitsethi786@gmail.com>
2026-07-25 09:41:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inorder Traversal and BST/inorder-traversal-and-bst.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool doesRepresentBST(vector<int> &arr) {
+        // code here
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i]>arr[i+1]) return false;
+        }
+        return true;
+        
+    }
+};
